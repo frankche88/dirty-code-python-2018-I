955fddb9e546de3cdbd9887672817ee63fc7be89
frankche88 <frank.che88@gmail.com>
2018-04-30 14:32:52 -0500

    dirty code

--- dirtycode/infrastructure.py
+++ dirtycode/infrastructure.py
@@ -8,9 +8,6 @@ class IRepository(object):
     def saveSpeaker(self, speaker):
         pass
 
-    def getRegistrationFee(self, experience):
-        pass
-
 class SqlServerRepository(IRepository):
     '''
     classdocs
@@ -25,21 +22,3 @@ class SqlServerRepository(IRepository):
     def saveSpeaker(self, speaker):
         # TODO: Save speaker to SQL Server DB. For now, just assume success and return 1.
         return 1;
-
-    def getRegistrationFee(self, experience):
-        if (experience <= 1):
-            return 500;
-        
-        if (experience >= 2 and experience <= 3):
-            return 250;
-        
-        
-        if (experience >= 4 and experience <= 5):
-            return 100;
-        
-        
-        if (experience >= 6 and experience <= 9):
-            return 50;
-        
-        
-        return 0;
